70f259ca45bd3c8aa2129f434ac926a4b715e7b9
pythonhack76 <lrulvoni@gmail.com>
2024-06-02 14:44:34 +0200

    update auth e main

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models import Base, Subscription, SessionLocal, engine
 from pydantic import BaseModel
+from datetime import datetime, timedelta
 from typing import List
+from auth import authenticate_user, create_access_token, get_current_active_user, Token
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
 
 # Dependency
 def get_db():
